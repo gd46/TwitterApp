d04bb15bdd3198eacf6f97968a91968f854f6c96
Giuseppe DiBella <giuseppedb@Giuseppes-MacBook-Pro-2.local>
2014-10-25 12:01:51 -0400

    Added requestedMethod, and set getFields

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
+	require_once('TwitterAPIExchance.php');
+	require_once('settings.php');
+
 	echo '<h2>Twitter APP</h2>';
+
+	$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+	$requestMethod = "GET";
+
+	$getfield = '?screen_name=gdibella21&count=20';
+
+	$twitter = new TwitterAPIExchance($settings);
+
+	echo $twitter->setGefield($getfield)->buildOauth($url, $requestMethod)->performRequest();
 ?>
