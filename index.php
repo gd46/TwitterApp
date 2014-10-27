0dc3939b612f10d55ca5d2540e03e4d8ec0eccf7
Giuseppe DiBella <giuseppedb@new-host-3.home>
2014-10-27 18:22:24 -0400

    First attempt at using the abraham twitteroauth library

--- index.php
+++ index.php
@@ -9,19 +9,25 @@
 	spl_autoload_register('Autoloader::loader');
 
 	//Set the oAuth and API Keys
-	$settings = Classes\Twitter\config\settings::twitterAccess();
-	$twitter = new Classes\Twitter\TwitterAPIExchange($settings);
+	//$settings = Classes\Twitter\config\settings::twitterAccess();
+	//$twitter = new Classes\Twitter\TwitterAPIExchange($settings);
 
 	echo '<h2>Twitter APP</h2>';
+
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
+
+	$content = $connection->get('status/user_timeline', array('screen_name' => 'gdibella21'));
+
+	print_r($content);
 
 	//newOperation is the twitter call for pulling recents tweets from timeline.
 	
 	//$twitter->get('https://api.twitter.com/1.1/statuses/user_timeline.json', '?screen_name=gdibella21&count=20');
 
-	$newOperation = new Classes\Twitter\setAPI();
-	$newOperation->setRequest('https://api.twitter.com/1.1/statuses/user_timeline.json','GET','?screen_name=gdibella21&count=20');
+	//$newOperation = new Classes\Twitter\setAPI();
+	//$newOperation->setRequest('https://api.twitter.com/1.1/statuses/user_timeline.json','GET','?screen_name=gdibella21&count=20');
 	//Returns a JSON response from Twitter
-	$string = json_decode($twitter->setGetfield($newOperation->getfield)->buildOauth($newOperation->url, $newOperation->requestMethod)->performRequest(), $assoc = TRUE);
+	//$string = json_decode($twitter->setGetfield($newOperation->getfield)->buildOauth($newOperation->url, $newOperation->requestMethod)->performRequest(), $assoc = TRUE);
 
 	/* if($string["errors"][0]["message"] != ""){
 		echo "<h3>Sorry, there was a problem.</h3>
@@ -29,5 +35,5 @@
 		.$string[errors][0]["message"]."</em></p>"; exit();
 	}*/
 
-	Classes\Html\htmlFunctions::printTable($string, 'horizontal');
+	//Classes\Html\htmlFunctions::printTable($string, 'horizontal');
 ?>
