4a96166f984cde95507f1ab469ef3fb31ed92da2
Giuseppe DiBella <giuseppedb@new-host-3.home>
2014-10-27 20:01:08 -0400

    Starting printing response in table

--- Classes/Html/htmlFunctions.php
+++ Classes/Html/htmlFunctions.php
@@ -1,23 +1,30 @@
 <?php
 
 
-	namespace Classes\Html;
+	//namespace Classes\Html;
 
 	class htmlFunctions{
 
-		public static $user_timeline_headings = ['Time and Date of Tweet','Tweet','Tweeted by','Screen name','Followers','Friends','Listed'];
+		public static $user_timeline_headings = ['Time and Date of Tweet','Tweet','Screen name','Followers','Friends','Listed'];
 		
-		public static function printVerticaltable($string){
+		public static function printVerticaltable($content){
 			$table = '<div id="verticalTable"><table border="1">';
-				foreach($string as $items){
-					$table .= '<tr><th>' . self::$user_timeline_headings[0] . '</th><td>' . $items['created_at'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[1] . '</th><td>' . $items['text'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[2] . '</th><td>' . $items['user']['name'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[3] . '</th><td>' . $items['user']['screen_name'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[4] . '</th><td>' . $items['user']['followers_count'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[5] . '</th><td>' . $items['user']['friends_count'] . '</td></tr>';
-					$table .= '<tr><th>' . self::$user_timeline_headings[6] . '</th><td>' . $items['user']['listed_count'] . '</td></tr>';
-				}
+				/*foreach($string as $items){
+					$table .= '<tr><th>' . self::$user_timeline_headings[0] . '</th><td>' . $items->created_at . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[1] . '</th><td>' . $items->text . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[2] . '</th><td>' . $items->screen_name . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[3] . '</th><td>' . $items->followers_count . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[4] . '</th><td>' . $items->friends_count . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[5] . '</th><td>' . $items->listed_count . '</td></tr>';
+				}*/
+
+					$table .= '<tr><th>' . self::$user_timeline_headings[0] . '</th><td>' . $content->created_at . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[1] . '</th><td>' . $content->text . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[2] . '</th><td>' . $content->screen_name . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[3] . '</th><td>' . $content->followers_count . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[4] . '</th><td>' . $content->friends_count . '</td></tr>';
+					$table .= '<tr><th>' . self::$user_timeline_headings[5] . '</th><td>' . $content->listed_count . '</td></tr>';
+			
 				$table .= '</table></div>';
 				echo $table; 
 		}
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 	require_once('Classes/Twitter/twitteroauth/TwitterOAuth.php');
 	require_once('Classes/Twitter/config/config.php');
-	require_once('Classes/Html/htmlFunctions.php');
+	require('Classes/Html/htmlFunctions.php');
 	
 
 	//Set the oAuth and API Keys
@@ -22,8 +22,6 @@
 	$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
 
 	$content = $connection->get('users/show', array('screen_name' => 'gdibella21'));
-
-	var_dump($content);
 
 	//newOperation is the twitter call for pulling recents tweets from timeline.
 	
@@ -39,27 +37,12 @@
 		<p>Twiter returned the following error message:</p><p><em>"
 		.$string[errors][0]["message"]."</em></p>"; exit();
 	}*/
-	$string = json_decode($content);
-	foreach($string as $items){
-		$test = $content['user']['screen_name'];
-		echo $test;
-	}
-	
-	/*$user_timeline_headings = ['Time and Date of Tweet','Tweet','Tweeted by','Screen name','Followers','Friends','Listed'];
-		
-			$table = '<div id="verticalTable"><table border="1">';
-				foreach($content as $items){
-					$table .= '<tr><th>' . $user_timeline_headings[0] . '</th><td>' . $items['created_at'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[1] . '</th><td>' . $items['text'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[2] . '</th><td>' . $items['user']['name'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[3] . '</th><td>' . $items['user']['screen_name'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[4] . '</th><td>' . $items['user']['followers_count'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[5] . '</th><td>' . $items['user']['friends_count'] . '</td></tr>';
-					$table .= '<tr><th>' . $user_timeline_headings[6] . '</th><td>' . $items['user']['listed_count'] . '</td></tr>';
-				}
-				$table .= '</table></div>';
-				echo $table; */
-		
+
+	echo $content->text;
+	var_dump($content);
+
 
 	//Classes\Html\htmlFunctions::printTable($content, 'horizontal');
+
+	htmlFunctions::printVerticaltable($content);
 ?>
